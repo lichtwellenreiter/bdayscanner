f39631cd7a071f3502f969fe0bd71db65e337be4
Florian Thiévent <florianjeremias@gmail.com>
2020-07-07 17:03:57 +0200

    Initial Commit

---
+++ BDayScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BDayScanner
+//
+//  Created by Florian Thiévent on 07.07.20.
+//  Copyright © 2020 Florian Thiévent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
